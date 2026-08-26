6fdec197e24dc5c09a8aa3463e17b45c147ede04
lucero <joserl.lucero.17@gmail.com>
2026-08-25 21:54:19 -0600

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://192.168.1.9:5173'],
+    methods: 'GET,PUT,POST,DELETE,PATCH',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
