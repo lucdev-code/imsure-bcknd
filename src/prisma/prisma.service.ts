6ceced4bb727fbd4a33faf86cc62893ac71b0a96
lucero <joserl.lucero.17@gmail.com>
2026-09-05 12:04:10 -0600

    logs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,8 +6,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit
-{
+  implements OnModuleInit {
   constructor(configService: ConfigService) {
     const connectionString =
       configService.get<string>('DATABASE_URL');
@@ -19,6 +18,16 @@ export class PrismaService
     }
 
     console.log('DATABASE_URL existe:', !!connectionString);
+
+    try {
+      const parsedUrl = new URL(connectionString);
+
+      console.log('DATABASE HOST:', parsedUrl.hostname);
+      console.log('DATABASE PORT:', parsedUrl.port || '5432');
+      console.log('DATABASE NAME:', parsedUrl.pathname);
+    } catch {
+      throw new Error('DATABASE_URL tiene un formato inválido');
+    }
 
     const adapter = new PrismaPg({
       connectionString,
